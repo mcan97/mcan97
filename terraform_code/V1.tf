d67dfdaba52685f76e7f10aea30b3f67b275e368
mcan97 <mehmetcanaydinyer@gmail.com>
2024-12-27 18:00:29 +0300

    new commit

--- terraform_code/V1.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "eu-central-1"
-}
-
-resource "aws_instance" "demo-server" {
-   ami = "ami-0e54671bdf3c8ed8d"  # AMI ID, us-east-1 bölgesine ait olmalı
-   instance_type = "t2.micro"
-   key_name      = "mcan97"  # AWS'de önceden oluşturulmuş bir keypair olmalı
-}
