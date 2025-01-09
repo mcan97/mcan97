8a1dcb43b6fbf2beda620992ce1eb7befe82317a
mcan97 <mehmetcanaydinyer@gmail.com>
2025-01-09 11:51:29 +0300

    added EKS module to terraform code

--- terraform_code/vpc/V4-EC2.tf
+++ terraform_code/vpc/V4-EC2.tf
@@ -108,3 +108,14 @@ resource "aws_route_table_association" "mcan97-rta-public-subnet-02" {
   subnet_id      = aws_subnet.mcan97-public-subnet-02.id
   route_table_id = aws_route_table.mcan97-public-rt.id
 }
+  module "sgs" {
+    source = "../sg_eks"
+    vpc_id     =     aws_vpc.mcan97-vpc.id
+ }
+
+  module "eks" {
+       source = "../eks"
+       vpc_id     =     aws_vpc.mcan97-vpc.id
+       subnet_ids = [aws_internet_gateway.mcan97-igw.id,aws_subnet.mcan97-public-subnet-02.id]
+       sg_ids = module.sgs.security_group_public
+ }
